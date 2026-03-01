710aa9adef0baa73b01434d330f9f7f677fada18
Aleshka228pro <alexeykaluz@gmail.com>
2026-03-01 15:42:25 +1000

    Delete src directory

--- src/lab_5_theme_8/EmptyException.java
+++
@@ -1,7 +0,0 @@
-package lab_5_theme_8;
-
-public class EmptyException extends Exception {
-    public EmptyException() {
-        super("container is empty");
-    }
-}
--- src/lab_5_theme_8/FullException.java
+++
@@ -1,7 +0,0 @@
-package lab_5_theme_8;
-
-public class FullException extends Exception{
-    public FullException(int maxSize) {
-        super("container is full");
-    }
-}
